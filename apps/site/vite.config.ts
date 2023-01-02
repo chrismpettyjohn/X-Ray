c3f4501436c8030af227210116efe1fbddc13f35
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-01-02 15:28:14 -0500

    Enable SSL support on Vite

--- apps/site/vite.config.ts
+++ apps/site/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: '../ssl/key.pem',
+      cert: '../ssl/cert.pem'
+    },
+  },
   plugins: [react()],
 })
